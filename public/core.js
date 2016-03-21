f47c3fb8460e59e78428b4c28576749d25433e32
paliy <georgi.paliy@gmail.com>
2016-03-21 03:17:31 +0200

    add modal window and css style

--- public/core.js
+++ public/core.js
@@ -24,7 +24,15 @@ var myapp = angular.module('myapp', ['ui.bootstrap']).controller('mainController
       .success(function(data) {
         $scope.formData = {};
         $scope.customer = data;
-        $scope.customers();
+        
+        $http.get('/customer')
+        .success(function(data) {
+          $scope.customers = data;
+          console.log(data);
+        })
+        .error(function(data) {
+          console.log('Error: ' + data);
+        });
         console.log(data);
       })
       .error(function(data) {
@@ -36,43 +44,54 @@ var myapp = angular.module('myapp', ['ui.bootstrap']).controller('mainController
   $scope.deleteCustomer = function(id) {
     $http.delete('/customer/', {params: {_id: id}})
       .success(function(data) {
-        $scope.customers();
+        $http.get('/customer')
+        .success(function(data) {
+          $scope.customers = data;
+          console.log(data);
+        })
+        .error(function(data) {
+          console.log('Error: ' + data);
+        });
       })
       .error(function(data) {
         console.log('Error: ' + data);
       });
   };
 
-  $scope.open = function(u){
+
+  $scope.open = function(customer){
     $uibModal.open({
       templateUrl: 'myModalContent.html',
       replace: true,
       backdrop: true,
       windowClass: 'modal',
       controller: function ($scope, $uibModalInstance, $log) {
-        $scope.customer = u;
+        $scope.customer = customer;
         $scope.editCustomer = function() {
           $http.put('/customer/', {
             params: {
               name: {
-                first: u.name.first,
-                last: u.name.last
+                first: customer.name.first,
+                last: customer.name.last
               },
-              id: u._id,
-              skype: u.skype,
-              companyName: u.companyName,
+              id: customer._id,
+              skype: customer.skype,
+              companyName: customer.companyName,
               phone: {
-                work: u.phone.work,
-                mobile: u.phone.mobile
+                work: customer.phone.work,
+                mobile: customer.phone.mobile
               }              
             }
           })
-            .success(function(data) {
-              console.log(data);
-            })
-            .error(function(data) {
-              console.log('Error: ' + data);
-            });
+          .success(function(data) {
+            console.log(data);
+          })
+          .error(function(data) {
+            console.log('Error: ' + data);
+          });
+        };
+        $scope.close = function () {
+         $uibModalInstance.close();
         };
       },
       resolve: {
